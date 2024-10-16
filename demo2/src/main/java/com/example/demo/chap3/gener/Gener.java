3b91d4bd96b72db6a0a234a098cad56f118701eb
thanhph25817 <110156996+thanhph25817@users.noreply.github.com>
2024-10-16 16:17:30 +0700

    set ,list , map

--- demo2/src/main/java/com/example/demo/chap3/gener/Gener.java
+++ demo2/src/main/java/com/example/demo/chap3/gener/Gener.java
@@ -32,6 +32,7 @@ public class Gener {
 
     }
 
+
     public static void printOb(List<? super Object> list) {
         list.add("s");
     }
@@ -41,19 +42,58 @@ public class Gener {
 //        List<String> list = new ArrayList<>();
 //        list.add("java");
 //        printList(list);
-        List<Dog> dog=new ArrayList<>();
-        printSub1(dog);
+//        List<Dog> dog = new ArrayList<>();
+//        printSub1(dog);
 
-        List<Integer> listInter = new ArrayList<>();
-        listInter.add(12);
-        printList(listInter);
-        printSub(listInter);
+//        List<Integer> listInter = new ArrayList<>();
+//        listInter.add(12);
+//        printList(listInter);
+//        printSub(listInter);
+//
+//        List<Long> longs = new ArrayList<>(10);
+//        longs.add(1l);
+//        printList(longs);
+//        printSub(longs);
+//
+//        List<String> list = new ArrayList<>();
+//        list.add(("as")); // true
+//        list.add(("assd"));
+//        System.out.println(list.remove("as")); // true
+//        System.out.println(list.isEmpty()); //false
+//        System.out.println(list.size()); //1
+//        list.clear();
+//        System.out.println(list.isEmpty()); // true
+//
+//        Set<Integer> set = new HashSet<>();
+//        boolean b1 = set.add(66); // true
+//        boolean b2 = set.add(10); // true
+//        boolean b3 = set.add(66); // false
+//        boolean b4 = set.add(8); // true
+//        for (Integer integer : set) System.out.print(integer + ","); // 66,8,10
+//
+//        Queue<Integer> queue=new ArrayDeque<>();
+//        queue.offer(12);
+//        queue.offer(13);
+//        queue.offer(14);
+//        System.out.println(queue.poll());
+//        System.out.println(queue.peek());
+//        System.out.println(queue);
+//
+//        ArrayDeque<Integer> arrayDeque=new ArrayDeque<>();
+//        arrayDeque.push(12);
+//        arrayDeque.push(13);
+//        System.out.println(arrayDeque); //13,12
+//        arrayDeque.poll();
+//        System.out.println(arrayDeque); //12
+//        arrayDeque.push(14);
+//        System.out.println(arrayDeque.peek());//14
 
-        List<Long> longs = new ArrayList<>(10);
-        longs.add(1l);
-        printList(longs);
-        printSub(longs);
-
+        Map<String,String> map=new TreeMap<>();
+        map.put("koala", "bamboo");
+        map.put("lion", "meat");
+        map.put("giraffe", "leaf");
+        for (String key: map.keySet())
+            System.out.print(key + ","); // giraffe,koala,lion,
 
 
     }
